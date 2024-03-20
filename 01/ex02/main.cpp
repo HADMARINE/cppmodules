89bd8e76d6df79bfb1725d12e72a6f70a4779df8
HADMARINE <joonlee031@live.com>
2024-03-20 02:18:29 +0100

    Update

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 19:46:21 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/03/19 19:50:38 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/03/20 02:17:56 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,8 @@ int main()
     std::string *stringPTR = &str;
     std::string &stringREF = str;
     
-    std::cout << "str: \n\n" << &str << std::endl;
-    std::cout << "stringPTR: \n\n" << &stringPTR << std::endl;
-    std::cout << "stringREF: \n\n" << &stringREF << std::endl;
+    std::cout << "str: \t\t" << &str << ":" << str << std::endl;
+    std::cout << "stringPTR: \t" << stringPTR << ":" << *stringPTR << std::endl;
+    std::cout << "stringREF: \t" << &stringREF << ":" << stringREF << std::endl;
     return (0);
 }
-
