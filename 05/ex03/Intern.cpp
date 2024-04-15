444c374435b5498b62af7c5f95d1d4001c34b051
HADMARINE <joonlee031@live.com>
2024-04-15 10:15:13 +0200

    05 fini

--- 05/ex03/Intern.cpp
+++ 05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 17:00:06 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/04/09 18:05:49 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/04/15 10:04:24 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,28 @@ Intern & Intern::operator=(const Intern & cpy) {
     return (*this);
 }
 
-AForm * makeForm(const std::string & request, const std::string & target) {
+static AForm * createPresidentialPardonForm(const std::string & target) {
+    return (new PresidentialPardonForm(target));
+}
+
+static AForm * createRobotomyRequestForm(const std::string & target) {
+    return (new RobotomyRequestForm(target));
+}
+
+static AForm * createShrubberyCreationForm(const std::string & target) {
+    return (new ShrubberyCreationForm(target));
+}
+
+AForm * Intern::makeForm(const std::string & request, const std::string & target) {
     std::string lst[3] = {"presidential pardon", "robotomy request", "shrubbery creation"};
-    const CF* forms[3] = {PresidentialPardonForm::new};
-    AForm *form;
+    AForm * (*func[3])(const std::string & target) = { createPresidentialPardonForm,
+                                                    createRobotomyRequestForm,
+                                                    createShrubberyCreationForm};
     for (int i = 0; i < 3; i++) {
         if (lst[i] == request) {
-            form = new 
-            return ;
+            return func[i](target);
+            
         }
     }
+    return (NULL);
 }
--- 05/ex03/main.cpp
+++ 05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 12:22:13 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/04/09 16:52:07 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/04/15 10:13:40 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include  "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "Intern.hpp"
 #include <iostream>
 
 int main()
@@ -21,16 +22,17 @@ int main()
     try {
         Bureaucrat b1("1", 1);
         Bureaucrat b2("2", 120);
-        PresidentialPardonForm f1("test1");
-        RobotomyRequestForm f2("test2");
-        ShrubberyCreationForm f3("test3");
+        Intern i1;
+        AForm *f1 = i1.makeForm("presidential pardon", "test1");
+        AForm *f2 = i1.makeForm("robotomy request", "test2");
+        AForm *f3 = i1.makeForm("shrubbery creation", "test3");
         
-        b1.signForm(f1);
-        b1.signForm(f2);
-        b1.signForm(f3);
-        b1.executeForm(f1);
-        b1.executeForm(f2);
-        b1.executeForm(f3);
+        b1.signForm(*f1);
+        b1.signForm(*f2);
+        b1.signForm(*f3);
+        b1.executeForm(*f1);
+        b1.executeForm(*f2);
+        b1.executeForm(*f3);
         // b2.signForm(f2);
     } catch (std::exception & e) {
         std::cout << e.what() << std::endl;
