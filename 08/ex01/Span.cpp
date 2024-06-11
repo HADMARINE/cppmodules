7ae1183ae04e6241031a049a4fa30fbb87ffa163
HADMARINE <joonlee031@live.com>
2024-06-12 00:51:36 +0200

    Meet requirements

--- 08/ex01/Span.cpp
+++ 08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 17:47:04 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/04/15 18:03:35 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/06/12 00:49:07 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,54 @@
 #include <climits>
 #include <iostream>
 
-Span::Span() : _arr(new int[0]), _len(0), _max_len(0) {}
+Span::Span() : _arr(std::vector<int>(0)), _len(0), _max_len(0) {}
 
-Span::Span(unsigned int n) : _arr(new int[n]), _len(0), _max_len(n) {}
+Span::Span(size_t n) : _arr(std::vector<int>(n)), _len(0), _max_len(n) {}
+
+Span::Span(const Span & cpy) { operator=(cpy); }
+
+Span::~Span() {}
 
 Span & Span::operator=(const Span &copy) {
     if (this == &copy)
         return *this;
-    delete[] _arr;
-    _arr = new int[copy._len];
-    for (unsigned int i = 0; i < copy._len; i++)
-        _arr[i] = copy._arr[i];
+    _arr = copy._arr;
     _len = copy._len;
+    _max_len = copy._max_len;
     return *this;
 }
 
 void Span::addNumber(int n) {
-    if (_len == _max_len)
-        throw std::exception();
-    if (_len == 0) {
-        _arr[_len++] = n;
-    } else {
-        unsigned int i;
-        for (i = 0; i < _len && _arr[i] < n; i++);
-        for (unsigned int j = _len; j > i; j--)
-        {
-            _arr[j] = _arr[j - 1];
-        }
-        _arr[i] = n;
-        _len++;
+    if (_len == _max_len) {
+        throw MaxCapacityReachedException();
     }
+    _arr[_len++] = n;
+    std::sort(_arr.begin(), _arr.begin() + _len);
 }
 
-unsigned int Span::shortestSpan() {
-    unsigned int span = UINT_MAX;
-    for (unsigned int i = 0; i < _len - 1; i++) {
-        if ((unsigned int)(_arr[i + 1] - _arr[i]) < span)
+int Span::shortestSpan() {
+    if (_len < 1)
+        throw RangeInvalidException();
+    int span = INT_MAX;
+    for (size_t i = 0; i < _len - 1; i++) {
+        if ((_arr[i + 1] - _arr[i]) < span)
             span = _arr[i + 1] - _arr[i];
     }
     return span;
 }
 
-unsigned int Span::longestSpan() {
-    return (unsigned int)(_arr[_len - 1] - _arr[0]);
+int Span::longestSpan() {
+    return *std::max_element(_arr.begin(), _arr.end()) - *std::min_element(_arr.begin(), _arr.end());
+}
+
+const char * Span::ElementAlreadyExistException::what() throw() {
+    return "Element already exists";
+}
+
+const char * Span::MaxCapacityReachedException::what() throw() {
+    return "Max capacity reached";
+}
+
+const char * Span::RangeInvalidException::what() throw() {
+    return "Range invalid";
 }
--- 08/ex01/Span.hpp
+++ 08/ex01/Span.hpp
@@ -6,28 +6,47 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 16:49:01 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/04/15 17:52:48 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/06/12 00:42:12 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <vector>
+#include <cstddef>
+#include <algorithm>
+
 class Span {
+public:
+    std::vector<int> _arr;
+    size_t _len;
+    size_t _max_len;
 private:
-    int *_arr;
-    unsigned int _len;
-    unsigned int _max_len;
 public:
     Span();
-    Span(const Span &copy) { operator=(copy);};
-    Span(unsigned int n);
-    ~Span() { delete[] _arr; };
+    Span(const Span &copy);
+    Span(size_t n);
+    ~Span();
     Span &operator=(const Span &copy);
     
     void addNumber(int n);
-    unsigned int shortestSpan();
-    unsigned int longestSpan();
+    
+    void addNumber(typename std::vector<int>::iterator start, std::vector<int>::iterator end);
+    int shortestSpan();
+    int longestSpan();
+
+    class MaxCapacityReachedException : public std::exception {
+        virtual const char * what() throw();
+    };
+
+    class ElementAlreadyExistException : public std::exception {
+        virtual const char * what() throw();
+    };
+
+    class RangeInvalidException : public std::exception {
+        virtual const char * what() throw();
+    };
 };
 
 #endif
